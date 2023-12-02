68dcc560f027f899a7e00223c840f39b45408448
KeplerInCoding <shreyakv8@gmail.com>
2023-12-02 23:59:37 +0530

    .

--- src/redux/slices/CounterSlice.js
+++ src/redux/slices/CounterSlice.js
@@ -1,20 +1,7 @@
-// import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 
-// const CounterSlice = createSlice();
+const CounterSlice = createSlice();
 
-// // export default CounterSlice;
+export default CounterSlice;
 
-// // // Path: src/redux/store.js
-
-// // import { configureStore } from "@reduxjs/toolkit";
-
-
-// const store = configureStore({
-//     reducer: {
-//         counter: CounterSlice.reducer,
-//     },
-//     });
-// export default store;
-
-// // Path: src/App.js
